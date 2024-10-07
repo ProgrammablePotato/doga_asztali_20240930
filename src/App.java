dfcd814dedb0216acfd8f648074265d584789939
ProgrammablePotato <tamas.mihelik@gmail.com>
2024-10-07 17:57:58 +0200

    3

--- src/App.java
+++ src/App.java
@@ -1,3 +1,13 @@
+/*
+* File: App.java
+* Author: Mihelik Tamás
+* Copyright: 2021, Mihelik Tamás
+* Date: 2024-10-07
+* Web: -
+* Licenc: MIT
+*
+*/
+
 public class App {
     public static void main(String[] args) throws Exception {
         Network nw = new Network();
